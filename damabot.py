e20661655166c75a12dc3623cf63e909cfaa233e
Censacrof <galisifrancesco@gmail.com>
2020-08-04 03:41:19 +0200

    Aggiunto il comando ruoli
    Co-authored-by: leonardo8b@live.it <leonardo8b@live.it>

--- damabot.py
+++ damabot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import logging
+import json
 
 
 # configuro il logger
@@ -31,6 +32,27 @@ async def clear(ctx):
 async def ruoli(ctx):
     """Genera ed invia il messaggio di selezione ruolo"""
     await ctx.message.delete()
+
+    # apro il file contenente le definizioni dei ruoli
+    roleDefinitions = None
+    try:
+        with open('roles.json', 'r') as roles_file:
+            roleDefinitions = json.load(roles_file)
+    except Exception as e:
+        msg = 'Errore: Impossibile fare il parsing dei ruoli\n' + ' - ' + str(e)
+        log.error(msg)
+        await ctx.send(msg)
+        return
+    
+    for group in roleDefinitions['groups']:
+        embed = discord.Embed(title=group['title'], description=group['description'], color=0x08457E)
+        
+        for role in group['roles']:
+            embed.add_field(name=role['title'], value=role['description'], inline=False)
+        mhvmsg = await ctx.send(embed=embed)
+        
+        for role in group['roles']:
+            await mhvmsg.add_reaction(role['emoji'])
     pass
 
 bot.run('NzM5NjYxNjUwOTUyNTg1Mjc3.Xydtlw.H6wzJOtzXu8elFVo-4SJ4jDvD-4')
